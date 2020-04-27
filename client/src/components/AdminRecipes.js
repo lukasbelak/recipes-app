d8721e7ead3304ae8574c28b89b845772679e88c
Lukas Belak <lukas.belak1@gmail.com>
2020-04-27 15:51:08 +0200

    added admin cat draft

---
+++ client/src/components/AdminRecipes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container,Header,Menu,Responsive,Image,Table,Checkbox,Button} from 'semantic-ui-react';
+
+const AdminRecipes=()=>{
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AdminRecipes;
--- client/src/containers/Admin.js
+++ client/src/containers/Admin.js
@@ -1,45 +1,42 @@
-import React,{useState} from 'react'
-import {Container,Header,Menu,Responsive,Image} from 'semantic-ui-react'
+import React,{useState} from 'react';
+import {Container,Header,Menu,Responsive,Image} from 'semantic-ui-react';
+import AdminCategories from '../components/AdminCategories';
+import AdminRecipes from '../components/AdminRecipes';
 
 const Admin = () => {
 
-  const [isHomeVisible, setIsHomeVisible]=useState(true);
-  const [isAboutVisible,setIsAboutVisible ]=useState(false);
+  const [isCategoriesVisible, setIsCategoriesVisible]=useState(true);
+  const [isRecipesVisible,setIsRecipesVisible ]=useState(false);
 
-  const handleHomeClick=()=>{
-    setIsHomeVisible(true);
-    setIsAboutVisible(false);
+  const handleCategoriesClick=()=>{
+    setIsCategoriesVisible(true);
+    setIsRecipesVisible(false);
   }
 
-  const handleAboutClick=()=>{
-    setIsHomeVisible(false);
-    setIsAboutVisible(true);
+  const handleRecipesClick=()=>{
+    setIsCategoriesVisible(false);
+    setIsRecipesVisible(true);
   }
 
   return(
     <div>
         <Responsive>
             <Menu fixed='top' inverted >
-            <Container>
-                <Menu.Item>
-                    <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
-                </Menu.Item>
-                <Menu.Item as='a' onClick={handleHomeClick}>Home</Menu.Item>
-                <Menu.Item as='a' onClick={handleAboutClick}>About</Menu.Item>
-            </Container>
+                <Container>
+                    <Menu.Item>
+                        <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+                    </Menu.Item>
+                    <Menu.Item as='a' onClick={handleCategoriesClick}>Categories</Menu.Item>
+                    <Menu.Item as='a' onClick={handleRecipesClick}>Recipes</Menu.Item>
+                </Container>
             </Menu>
 
-            <Container style={isHomeVisible?{ display: 'block',marginTop:'8em' }:{display:'none'}}>
-            <Header as='h1'>Semantic UI React Fixed Template</Header>
-            <p>This is a basic fixed menu template using fixed size containers.</p>
-            <p>
-                A text container is used for the main container, which is useful for single column layouts. A text container is used for the main container, which is useful for single column layouts. A text container is used for the main container, which is useful for single column layouts. A text container is used for the main container, which is useful for single column layouts.
-            </p>
+            <Container style={isCategoriesVisible?{ display: 'block',marginTop:'8em' }:{display:'none'}}>
+                <AdminCategories />
             </Container>
 
-            <Container style={isAboutVisible?{ display: 'block',marginTop:'8em' }:{display:'none'}}>
-            <Header as='h1'>Semantic</Header>
-            <p>This is a basic </p>
+            <Container style={isRecipesVisible?{ display: 'block',marginTop:'8em' }:{display:'none'}}>
+                <AdminRecipes />
             </Container>
         </Responsive>
     </div>
