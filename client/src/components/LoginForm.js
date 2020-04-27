51cf6139a90a70bce28cfa8e9c6e459c99f7ff3d
Lukas Belak <lukas.belak1@gmail.com>
2020-04-27 09:11:27 +0200

    added router draft

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,33 +1,63 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
 
-const LoginForm = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        <Image src='/logo.png' /> Log-in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
+const LoginForm = () => {
 
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <a href='#'>Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+    const [isRedirectToHome,setIsRedirectToHome]=useState(false);
 
-export default LoginForm
+    const redirectToHome=()=>{
+        if(isRedirectToHome){
+            return <Redirect to='/home' />;
+        }
+    }
+
+    const handleLogin=()=>{
+        debugger;
+        setIsRedirectToHome(true);
+    };
+
+    const userOnChange=(e)=>{
+
+    };
+
+    const passwordOnChange=(e)=>{
+
+    };
+
+
+    return(
+        <div>
+            {redirectToHome()}
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='olive' textAlign='center'>
+                Log-in to your account
+            </Header>
+            <Form size='large'>
+                <Segment stacked>
+                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' onChange={userOnChange} />
+                <Form.Input
+                    fluid
+                    icon='lock'
+                    iconPosition='left'
+                    placeholder='Password'
+                    type='password'
+                    onChange={passwordOnChange}
+                />
+
+                <Button color='olive' fluid size='large' onClick={handleLogin}>
+                    Login
+                </Button>
+                </Segment>
+            </Form>
+            <Message style={{width:"100%"}}>
+                New to us? <a href='/register'>Sign Up</a>
+            </Message>
+            </Grid.Column>
+        </Grid>
+        </div>
+    )
+};
+
+export default LoginForm;
---
+++ client/src/containers/Admin.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const Admin=()=>{
+
+    return (
+        <h1>Hello admin</h1>
+    );
+};
+
+export default Admin;
