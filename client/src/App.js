3192b916d5cbcd12d464095c6ac52255301406e2
Lukas Belak <lukas.belak1@gmail.com>
2020-05-01 21:11:58 +0200

    added redirect to /

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'semantic-ui-css/semantic.min.css';
@@ -13,6 +13,7 @@ const App=()=> {
   return (
     <Router>
       <Switch>
+        <Redirect from='/recipes-app' to='/' />>
         <Route path="/" exact component={LoginForm} />
         <Route path="/admin" exact component={Admin} />
         <Route path="/home" exact component={Home} />
