5b2913e019e8b8071b47666d7441ed5c6e1cb43a
Lukas Belak <lukas.belak1@gmail.com>
2023-05-17 14:09:28 +0200

    Updates

---
+++ precache-manifest.23df791e0459a7d546e7273ef666f4b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9c4525b6f9ae2e6399cc1dcc77e6cb",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "bb58c668425941342eaa",
+    "url": "/recipes-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b409055ce4a04d056fb",
+    "url": "/recipes-app/static/js/2.537f5a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-app/static/js/2.537f5a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb58c668425941342eaa",
+    "url": "/recipes-app/static/js/main.0ea83092.chunk.js"
+  },
+  {
+    "revision": "58699139534697a91c22",
+    "url": "/recipes-app/static/js/runtime-main.3ca22642.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipes-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
