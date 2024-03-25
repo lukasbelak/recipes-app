4c38829324ea111c5fac02fa5af2dc08c26b1370
Lukáš Belák <lukas.belak1@gmail.com>
2024-03-25 08:55:50 +0100

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import './libraries/bootstrap/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
+// import $ from 'jquery';
 import 'semantic-ui-css/semantic.min.css';
-import 'semantic-ui-css/semantic.js';
+// import 'semantic-ui-css/semantic.js';
 import 'hover.css';
 import './App.css';
 import Home from './containers/Home';
 import Admin from './containers/Admin';
 import LoginForm from './components/Login/LoginForm';
 import ErrorPage from './containers/ErrorPage'; //
+// window.jQuery = $;
+// window.$ = $;
+// global.jQuery = $;
 
 const App=()=> {
 
